0bf8bc6238ef0e8aabdef3fe444b1610963c3ed6
Sergey Lebedkin <s.lebedkin@gmail.com>
2020-01-05 17:21:00 +0200

    initial commit with base ecosystem for vs code

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(int argc, char** argv)
+{
+    std::cout << "THis is the new string!!" << std::endl;
+    std::cout << "Hello, world!!!" << std::endl;
+    return 0;
+}
