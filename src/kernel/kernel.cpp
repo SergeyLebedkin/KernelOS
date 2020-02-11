7f0420b7211c77074743ebe306f56822dd6233e1
Sergey Lebedkin <s.lebedkin@gmail.com>
2020-02-11 23:38:31 +0200

    cleanup

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,13 +1,3 @@
-class AAA {
-private:
-    int val0;
-    int val1;
-    int val2;
-public:
-    AAA() {}
-    ~AAA() {}
-};
-
 // kernel_main
 extern "C" void kernel_main(){
     unsigned long long* vid_mem = (unsigned long long *)0xb8000;
