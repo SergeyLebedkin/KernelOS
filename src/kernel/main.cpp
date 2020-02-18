571c5495eb65f27609f355ed9a3e1eafcbe35638
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2020-02-18 15:58:23 +0200

    ostream prototype

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,7 +1,8 @@
 #include <cinttypes>
+#include <ostream>
 
 // main
 extern "C" void kmain() {
-    uint64_t* vid_mem = (uint64_t *)0xb8000;
+    uint64_t *vid_mem = (uint64_t *)0xb8000;
     vid_mem[1] = 0x1F201F201F201F20;
 }
